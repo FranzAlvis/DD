0d3d0bfb0dc5cb7b86e7807a2d6eafbfaadf43b1
Franz Alvis <alvis.cornejo.franzreinaldo@usfx.bo>
2025-08-20 23:19:19 -0400

    otra

---
+++ 1-3-httpslow-slowloris.sh
@@ -0,0 +1,3 @@
+date >> test-mobile-stress.txt
+ab -n 10000 -c 100 -H "User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36" https://webabtest.oep.org.bo/
+date >> test-mobile-stress.txt
